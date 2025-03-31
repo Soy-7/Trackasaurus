7c59e187fa7f7abb2e6522c5cbaf9568dbdc8c0a
sai <pss2k5@gmail.com>
2025-03-31 11:06:51 +0530

    enhance Tailwind CSS configuration and add postcss-import plugin; update dependencies again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true, // Optional: Enables React Strict Mode
-  swcMinify: true, // Optional: Enables SWC-based minification
 };
 
 export default nextConfig;
